8c13ae68344297dc096432059656749d3de7831a
TinxG <53986122+TinxG@users.noreply.github.com>
2020-02-08 10:41:49 +0800

    change test.cpp

---
+++ C++/test.cpp
@@ -0,0 +1,12 @@
+#include "algo_test.h"
+#include "bubble_sort.h"
+
+using namespace std;
+using namespace algos;
+using namespace testhelper;
+
+int main() {
+	//在这里添加你想要的测试代码
+	system("pause");
+	return 0;
+}
