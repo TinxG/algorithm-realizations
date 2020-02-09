7500cd963f0f838e22dd96f4001e00047e3434b2
TinxG <53986122+TinxG@users.noreply.github.com>
2020-02-09 11:42:10 +0800

    add binarySearch floor and ceil

--- C++/binary_search.h
+++ C++/binary_search.h
@@ -45,4 +45,52 @@ namespace algos {
 	int binarySearchB(T arr[], int length, T target) {
 		return __binarySearch(arr, 0, length - 1, target);
 	}
+
+	//ceil，二分查找法的变种
+	//有重复目标元素时，查找到的是最后一个
+	//不存在目标时，查找到的是比目标大的最小元素
+	template <typename T>
+	int binarySearchCeil(T arr[], int length, T target) {
+		//区间[l,r)
+		int l = 0, r = length;
+		while (l < r) {
+			int mid = l + (r - l) / 2;
+			if (arr[mid] <= target) {
+				l = mid + 1;
+				continue;
+			}
+			else {
+				r = mid;
+				continue;
+			}
+		}
+		assert(l == r);
+		if (r - 1 >= 0 && arr[r - 1] == target)
+			return r - 1;
+		return r;
+	}
+
+	//floor，二分查找法的变种
+	//有重复目标元素时，查找到的是第一个
+	//不存在目标时，查找到的是比目标小的最后一个
+	template <typename T>
+	int binarySearchFloor(T arr[], int length, T target) {
+		//区间(l,r]
+		int l = -1, r = length - 1;
+		while (l < r) {
+			int mid = l + (r - l + 1) / 2;
+			if (arr[mid] >= target) {
+				r = mid - 1;
+				continue;
+			}
+			else {
+				l = mid;
+				continue;
+			}
+		}
+		assert(l == r);
+		if (l + 1 < length && arr[l + 1] == target)
+			return l + 1;
+		return l;
+	}
 }
--- C++/test.cpp
+++ C++/test.cpp
@@ -10,8 +10,9 @@ using namespace dataStruct;
 
 int main() {
 	//在这里添加你想要的测试代码
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	cout << arr[binarySearchB(arr, 10, 8)] << endl;
+	const int length = 14;
+	int arr[length] = { 1,2,3,4,5,8,8,8,8,8,8,9,9,10 };
+	cout << binarySearchFloor(arr, length, 8) << endl;
 	system("pause");
 	return 0;
 }
